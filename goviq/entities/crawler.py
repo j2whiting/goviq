2eab44529c498933101271b5c5040d4f2d90c18a
Julian Whiting <j2whitin@gmail.com>
2023-07-30 20:12:24 -0400

    refactor to contain defined functions shared between scrapers... WIP

--- goviq/entities/crawler.py
+++ goviq/entities/crawler.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod
+import aiohttp
+import asyncio
+import logging
 from typing import Any
 
 
@@ -10,31 +13,36 @@ class Crawler:
                  "AppleWebKit/605.1.15 (KHTML, like Gecko)" \
                  "Version/12.1.1 Safari/605.1.15"
 
-    @abstractmethod
-    async def _fetch(self, **kwargs) -> str:
-        """
-        Fetches text from a url.
-        :param kwargs:
-        :return: text body from URL
-        """
-        raise NotImplementedError()
+    async def _fetch(self, url: str, session: aiohttp.ClientSession):
+        async with session.get(url, headers={'User-Agent': self.user_agent}) as response:
+            if response.status == 200:
+                return await response.text()
+            logging.warning(
+                f"Failed to fetch page: {url} Status code: {response.status}"
+            )
+            return None
 
     @abstractmethod
-    async def _parse(self, **kwargs) -> Any:
+    def _parse(self, **kwargs) -> Any:
         """
         :param kwargs:
         :return:
         """
         raise NotImplementedError()
 
-    @abstractmethod
-    async def _crawl(self, **kwargs):
-        """
-        Crawls list of URLs using fetch and parse.
-        :param kwargs:
-        :return:
-        """
-        raise NotImplementedError()
+    async def _fetch_and_parse(self, page_link, session): # TODO: take this out of the class
+        logging.info(f'CRAWLING .... {page_link}')
+        try:
+            html = await self._fetch(page_link, session)
+        except aiohttp.ClientResponseError() as e:
+            logging.info(f"Error: {e}")
+            return None  # or handle this case accordingly
+        return {page_link: await self._parse(html)}
+
+    async def _crawl(self, links):
+        async with aiohttp.ClientSession() as session:
+            tasks = [self._fetch_and_parse(page_link, session) for page_link in links]
+            return await asyncio.gather(*tasks)
 
     @abstractmethod
     def _cache(self, **kwargs):
