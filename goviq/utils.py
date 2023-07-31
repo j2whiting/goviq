4b58f4a56882c3faab12234bd4fb073d414666a9
Julian Whiting <j2whitin@gmail.com>
2023-07-30 21:05:55 -0400

    general purpose html text extractor

--- goviq/utils.py
+++ goviq/utils.py
@@ -1,4 +1,5 @@
 import datetime
+import bs4
 
 
 def datestamp():
@@ -9,3 +10,11 @@ def datestamp():
 def dateparse(datestring):
     """Return a datetime object from a datestamp string in the format YYYYMMDDHHMMSS"""
     return datetime.datetime.strptime(datestring, "%Y%m%d%H%M%S")
+
+
+def extract_html_text(html: str):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    texts = soup.find_all(string=True)
+    visible_texts = filter(
+        lambda text: text.parent.name not in ['style', 'script', 'head', 'title', 'meta', '[document]'], texts)
+    return " ".join(t.strip() for t in visible_texts if t.strip())
