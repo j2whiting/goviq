97aa4f89a974aa6e96e2281973badff2f0a476e6
Julian Whiting <j2whitin@gmail.com>
2023-08-12 14:39:15 -0400

    WIP multiprocessed language detection and document splitting... slow and buggy

--- goviq/utils.py
+++ goviq/utils.py
@@ -1,5 +1,8 @@
+from concurrent.futures import ProcessPoolExecutor
 import datetime
 import bs4
+from langdetect import detect_langs
+import json
 
 
 def datestamp():
@@ -18,3 +21,41 @@ def extract_html_text(html: str):
     visible_texts = filter(
         lambda text: text.parent.name not in ['style', 'script', 'head', 'title', 'meta', '[document]'], texts)
     return " ".join(t.strip() for t in visible_texts if t.strip())
+
+
+def load_json_docs(path: str):
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
+def find_language_transition(text, window_size=5000, step_size=3000, batch_size=10):
+    num_windows = len(text) - window_size + 1
+    transition_index = None
+    min_diff = float('inf')
+
+    with ProcessPoolExecutor() as executor:
+        for i in range(0, num_windows, step_size):
+            batch_windows = [text[i + j:i + j + window_size] for j in range(0, step_size, batch_size)]
+            lang_probabilities_list = list(executor.map(detect_langs, batch_windows))
+
+            for lang_probabilities in lang_probabilities_list:
+                if len(lang_probabilities) >= 2:
+                    diff = abs(lang_probabilities[0].prob - lang_probabilities[1].prob)
+
+                    if diff < min_diff:
+                        min_diff = diff
+                        transition_index = i + window_size
+
+    return transition_index
+
+
+def split_document_at_language_transition(document, window_size=1000):
+    transition_index = find_language_transition(document, window_size)
+
+    if transition_index is not None:
+        english_text = document[:transition_index]
+        french_text = document[transition_index:]
+        return english_text, french_text
+    else:
+        return document, None
+
