cd0cd905f93aa02e956088192a3e4c4cee74e685
Julian Whiting <j2whitin@gmail.com>
2023-08-12 23:44:54 -0400

    remove bug capturing newline in titles

--- goviq/preprocessing/acts_ca.py
+++ goviq/preprocessing/acts_ca.py
@@ -1,5 +1,6 @@
 import os
 import regex as re
+import requests
 import tqdm
 from typing import List
 
@@ -12,23 +13,23 @@ class ActCAPreprocessor(Preprocessor):
     version = 1
     # Note: This regex seems to work for all acts except for 4 out of the ~900ish total federal acts.
     # I have not checked for false positives yet
-    act_title_regex = r'^(\n?[A-Z][a-z].+?)(?=\s?(?:R\.S\.C\.|S\.C\.|Agreements and ConventionsAssented))'
+    act_title_regex = r'^([A-Z][a-z].+?)(?=\s?(?:R\.S\.C\.|S\.C\.|Agreements and ConventionsAssented))'
 
     def __init__(self, local_cache: str = None):
         self.local_cache = local_cache if local_cache else LOCAL_CACHE
 
     def _parse_title(self, html_body: str) -> str:
         title = re.findall(self.act_title_regex, html_body, re.MULTILINE)
-        return title[0] if title else None
+        return title[0].strip() if title else None
 
-    def preprocess(self, path: str, cache: bool = True) -> List[dict]:
-        docs = self.load(path)
+    def preprocess(self, acts_path: str, cache: bool = True) -> List[dict]:
+        docs = self.load(acts_path)
         processed_docs = []
-        for doc in tqdm.tqdm(docs):
+        for doc in tqdm.tqdm(docs):  # TODO: Add multiprocessing
             json_doc = {}
             url = list(doc.keys())[0]
             html_body = list(doc.values())[0][0]
-            # TODO: Fix this, landetect not playing nicely with multiprocessing.
+            # TODO: Fix this, langdetect not playing nicely with multiprocessing.
             # html_body, _ = split_document_at_language_transition(html_body)
             title = self._parse_title(html_body)
             if title:
@@ -37,7 +38,7 @@ class ActCAPreprocessor(Preprocessor):
             json_doc['body'] = html_body
             processed_docs.append(json_doc)
         if cache:
-            out_path = os.path.join(self.local_cache, 'processed_' + path)
+            out_path = os.path.join(self.local_cache, 'processed_' + acts_path)
             self.cache(processed_docs, path=out_path)
         return processed_docs
 
